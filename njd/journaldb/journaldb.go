262bf75317be00db2248771f7e98f8cb8de69a9c
Ian Li <ian.li@gmx.com>
2018-11-02 07:39:55 +0800

    * Clean-up. * Added command line options for server files. * Save messages. * Add Git commit hook to run go fmt: https://golang.org/misc/git/pre-commit

--- njd/journaldb/journaldb.go
+++ njd/journaldb/journaldb.go
@@ -1,173 +1,183 @@
 package journaldb
 
 import (
-    "fmt"
-    "time"
-    "database/sql"
-    _ "github.com/mattn/go-sqlite3"
-    "github.com/shopspring/decimal"
-    //"encoding/json"
+	"database/sql"
+	"fmt"
+	_ "github.com/mattn/go-sqlite3"
+	"github.com/shopspring/decimal"
+	"time"
+	//"encoding/json"
 )
 
+
 type Diary struct {
-    Oid int
-    Date string
-    Content string
-    Highlighted bool
-    Added int
-    Updated int
+	Oid         int
+	Date        string
+	Content     string
+	Highlighted bool
+	Added       int
+	Updated     int
 }
 
-func (diary Diary) Save (dbconn *sql.DB) {
-    if diary.Oid > 0 {
-      // Update
-      stmt, err := dbconn.Prepare("UPDATE diary set content = ?, highlighted = ?, updated = strftime('%s', 'now') WHERE oid = ? and date = ?")
-      checkErr(err)
-      _, err = stmt.Exec(diary.Content, diary.Highlighted, diary.Oid, diary.Date)
-      checkErr(err)
-      stmt.Close()
-      fmt.Printf("Diary %d updated.\n", diary.Oid)
-    } else {
-      // INSERT
-      stmt, err := dbconn.Prepare("INSERT INTO diary(date, content, highlighted) VALUES(?,?,?)")
-      checkErr(err)
-      res, err := stmt.Exec(diary.Date, diary.Content, diary.Highlighted)
-      checkErr(err)
-      id, err := res.LastInsertId()
-      checkErr(err)
-      stmt.Close()
-      fmt.Printf("New diary %d saved to database.\n", id)
-    }
+func (diary Diary) Save(dbconn *sql.DB) {
+	if diary.Oid > 0 {
+		// Update
+		stmt, err := dbconn.Prepare("UPDATE diary set content = ?, highlighted = ?, updated = strftime('%s', 'now') WHERE oid = ? and date = ?")
+		checkErr(err)
+		_, err = stmt.Exec(diary.Content, diary.Highlighted, diary.Oid, diary.Date)
+		checkErr(err)
+		stmt.Close()
+		SaveMessage(dbconn, fmt.Sprintf("Diary %d updated.", diary.Oid))
+	} else {
+		// INSERT
+		stmt, err := dbconn.Prepare("INSERT INTO diary(date, content, highlighted) VALUES(?,?,?)")
+		checkErr(err)
+		res, err := stmt.Exec(diary.Date, diary.Content, diary.Highlighted)
+		checkErr(err)
+		id, err := res.LastInsertId()
+		checkErr(err)
+		stmt.Close()
+		SaveMessage(dbconn, fmt.Sprintf("New diary %d saved to database.", id))
+	}
 }
 
-func GetDiary (dbconn *sql.DB, diary *Diary) {
-  var date string
-  //fmt.Printf("SELECT date, content, highlighted FROM diary WHERE oid = %d \n", diary.Oid)
-  rows, err := dbconn.Query("SELECT date, content, highlighted FROM diary WHERE oid = ?", diary.Oid)
-  checkErr(err)
-  for rows.Next() {
-    err = rows.Scan(&date, &diary.Content, &diary.Highlighted)
-  }
-  t, _ := time.Parse(time.RFC3339, date)
-  diary.Date = t.Format("2006-01-02")
-  checkErr(err)
-  rows.Close()
+func GetDiary(dbconn *sql.DB, diary *Diary) {
+	var date string
+	//fmt.Printf("SELECT date, content, highlighted FROM diary WHERE oid = %d \n", diary.Oid)
+	rows, err := dbconn.Query("SELECT date, content, highlighted FROM diary WHERE oid = ?", diary.Oid)
+	checkErr(err)
+	for rows.Next() {
+		err = rows.Scan(&date, &diary.Content, &diary.Highlighted)
+	}
+	t, _ := time.Parse(time.RFC3339, date)
+	diary.Date = t.Format("2006-01-02")
+	checkErr(err)
+	rows.Close()
 }
-
 
 type Transaction struct {
-    Oid int
-    Date string
-    Item string
-    Description string
-    Amount decimal.Decimal
-    Pay bool
-    Income bool
-    Direction string
-    Currency int
-    CurrencyPrefix string
-    Payment int
-    PaymentName string
-    Bank int
-    BankName string
-    Added int
-    Updated int
+	Oid            int
+	Date           string
+	Item           string
+	Description    string
+	Amount         decimal.Decimal
+	Pay            bool
+	Income         bool
+	Direction      string
+	Currency       int
+	CurrencyPrefix string
+	Payment        int
+	PaymentName    string
+	Bank           int
+	BankName       string
+	Added          int
+	Updated        int
 }
 
-func (transaction Transaction) Save (dbconn *sql.DB) {
-    if transaction.Oid == 0 {
-      // INSERT
-      stmt, err := dbconn.Prepare("INSERT INTO transactions (date, item, description, currency, amount, pay, income, payment, bank) VALUES(?,?,?,?,?,?,?,?,?)")
-      checkErr(err)
-      res, err := stmt.Exec(transaction.Date, transaction.Item, transaction.Description, transaction.Currency, transaction.Amount, transaction.Pay, transaction.Income, transaction.Payment, transaction.Bank)
-      checkErr(err)
-      id, err := res.LastInsertId()
-      checkErr(err)
-      stmt.Close()
-      fmt.Printf("New transaction %d saved to database.\n", id)
-    }
-    // ToDo: Update
+func (transaction Transaction) Save(dbconn *sql.DB) {
+	if transaction.Oid == 0 {
+		// INSERT
+		stmt, err := dbconn.Prepare("INSERT INTO transactions (date, item, description, currency, amount, pay, income, payment, bank) VALUES(?,?,?,?,?,?,?,?,?)")
+		checkErr(err)
+		res, err := stmt.Exec(transaction.Date, transaction.Item, transaction.Description, transaction.Currency, transaction.Amount, transaction.Pay, transaction.Income, transaction.Payment, transaction.Bank)
+		checkErr(err)
+		id, err := res.LastInsertId()
+		checkErr(err)
+		stmt.Close()
+		SaveMessage(dbconn, fmt.Sprintf("New transaction %d saved to database.", id))
+	}
+	// ToDo: Update
 }
 
 type Currency struct {
-    Id int
-    Name string
-    Prefix string
-    Current bool
+	Id      int
+	Name    string
+	Prefix  string
+	Current bool
 }
 
-func GetCurrencies (dbconn *sql.DB) []Currency {
-    rows, err := dbconn.Query("SELECT id, name, prefix, current from currency WHERE id <> 0")
-    checkErr(err)
-    var currencies []Currency
-    for rows.Next() {
-      var currency Currency
-      err = rows.Scan(&currency.Id, &currency.Name, &currency.Prefix, &currency.Current)
-      checkErr(err)
-      currencies = append(currencies, currency)
-    }
-    rows.Close()
-    return currencies
+func GetCurrencies(dbconn *sql.DB) []Currency {
+	rows, err := dbconn.Query("SELECT id, name, prefix, current from currency WHERE id <> 0")
+	checkErr(err)
+	var currencies []Currency
+	for rows.Next() {
+		var currency Currency
+		err = rows.Scan(&currency.Id, &currency.Name, &currency.Prefix, &currency.Current)
+		checkErr(err)
+		currencies = append(currencies, currency)
+	}
+	rows.Close()
+	return currencies
 }
 
 type Payment struct {
-    Id int
-    Name string
-    Desc string
-    Priority bool
+	Id       int
+	Name     string
+	Desc     string
+	Priority bool
 }
 
-func GetPayments (dbconn *sql.DB) []Payment {
-    rows, err := dbconn.Query("SELECT id, name, description, priority FROM payment WHERE id <> 0")
-    checkErr(err)
-    var payments []Payment
-    for rows.Next() {
-      var payment Payment
-      err = rows.Scan(&payment.Id, &payment.Name, &payment.Desc, &payment.Priority)
-      checkErr(err)
-      payments = append(payments, payment)
-    }
-    rows.Close()
-    return payments
+func GetPayments(dbconn *sql.DB) []Payment {
+	rows, err := dbconn.Query("SELECT id, name, description, priority FROM payment WHERE id <> 0")
+	checkErr(err)
+	var payments []Payment
+	for rows.Next() {
+		var payment Payment
+		err = rows.Scan(&payment.Id, &payment.Name, &payment.Desc, &payment.Priority)
+		checkErr(err)
+		payments = append(payments, payment)
+	}
+	rows.Close()
+	return payments
 }
 
 type Bank struct {
-    Id int
-    Name string
-    Desc string
-    Priority bool
+	Id       int
+	Name     string
+	Desc     string
+	Priority bool
 }
 
-func GetBanks (dbconn *sql.DB) []Bank {
-    rows, err := dbconn.Query("SELECT id, name, description, priority from bank WHERE active")
-    checkErr(err)
-    var banks []Bank
-    for rows.Next() {
-      var bank Bank
-      err = rows.Scan(&bank.Id, &bank.Name, &bank.Desc, &bank.Priority)
-      checkErr(err)
-      banks = append(banks, bank)
-    }
-    rows.Close()
-    return banks
+func GetBanks(dbconn *sql.DB) []Bank {
+	rows, err := dbconn.Query("SELECT id, name, description, priority from bank WHERE active")
+	checkErr(err)
+	var banks []Bank
+	for rows.Next() {
+		var bank Bank
+		err = rows.Scan(&bank.Id, &bank.Name, &bank.Desc, &bank.Priority)
+		checkErr(err)
+		banks = append(banks, bank)
+	}
+	rows.Close()
+	return banks
 }
-
 
 func Open(DSN string) *sql.DB {
-    conn, err := sql.Open("sqlite3", DSN)
-    checkErr(err)
-    fmt.Printf("Dateabase '%s' opened for journaling.\n", DSN)
-    return conn
+	conn, err := sql.Open("sqlite3", DSN)
+	checkErr(err)
+	SaveMessage(conn, fmt.Sprintf("Dateabase '%s' opened for journaling.", DSN))
+	return conn
 }
 
 func Close(conn *sql.DB) {
-    err := conn.Close()
-    checkErr(err)
-    fmt.Printf("Dateabase closed.\n")
+	SaveMessage(conn, "Closing dateabase .")
+	err := conn.Close()
+	checkErr(err)
+}
+
+func SaveMessage(dbconn *sql.DB, msg string) {
+	now := time.Now()
+	fullMsg := fmt.Sprintf("%s: %s",now.Format(time.UnixDate), msg)
+	stmt, err := dbconn.Prepare("INSERT INTO message(added, content) VALUES(strftime('%s', 'now') ,?)")
+	checkErr(err)
+	_, err = stmt.Exec(fullMsg)
+	checkErr(err)
+	stmt.Close()
+	fmt.Printf("%s\n", fullMsg)
 }
 
 func checkErr(err error) {
-    if err != nil {
-        panic(err)
-    }
+	if err != nil {
+		panic(err)
+	}
 }
--- njd/njd.go
+++ njd/njd.go
@@ -1,240 +1,248 @@
 package main
 
 import (
-    "fmt"
-    "strings"
-    "time"
-    "io/ioutil"
-    "crypto/tls"
-    "crypto/x509"
-    "net/http"
-    "database/sql"
-    "html/template"
-    "log"
-    "github.com/shopspring/decimal"
-    "./journaldb"
+	"./journaldb"
+	"crypto/tls"
+	"crypto/x509"
+	"database/sql"
+	"flag"
+	"fmt"
+	"github.com/shopspring/decimal"
+	"html/template"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strings"
+	"time"
 )
 
-const timeFormat = "2006-01-02"
+const dateFormatShort = "2006-01-02"
+
+var (
+	dbFile           = flag.String("db", "journal.db", "SQLite DB file for journaling.")
+	serverCertFile   = flag.String("cert", "server.crt", "TLS server certification file.")
+	serverKeyFile    = flag.String("key", "server.key", "TLS server key file.")
+	clientCaCertFile = flag.String("ca", "ca.crt", "TLS client CA cert file.")
+	svrAddress       = flag.String("address", "0.0.0.0", "Listening address, default: 0.0.0.0")
+	svrPort          = flag.String("port", "8080", "Listening port, default: 80.")
+)
 
 type DiaryForm struct {
-    DBConn *sql.DB
-    Mode string
-    Message string
-    Diary []journaldb.Diary
-    Diary2Update journaldb.Diary
-    StartDate string
-    EndDate string
-    DayCount int
-    RowCount int
+	DBConn       *sql.DB
+	Mode         string
+	Message      string
+	Diary        []journaldb.Diary
+	Diary2Update journaldb.Diary
+	StartDate    string
+	EndDate      string
+	DayCount     int
+	RowCount     int
 }
 
 func (diaryForm *DiaryForm) Form(w http.ResponseWriter, r *http.Request) {
-    diaryForm.Mode = "New"
+	diaryForm.Mode = "New"
 
-    r.ParseForm()
+	r.ParseForm()
 
-    if diaryForm.StartDate = strings.Join(r.Form["s_date"], ""); diaryForm.StartDate == "" {
-        diaryForm.StartDate = getDateByDays(-7)
-    }
-    if diaryForm.EndDate = strings.Join(r.Form["e_date"], ""); diaryForm.EndDate == "" {
-        diaryForm.EndDate = getDateByDays(0)
-    }
-    diaryForm.DayCount = getDaysByDate(diaryForm.StartDate, diaryForm.EndDate)
+	if diaryForm.StartDate = strings.Join(r.Form["s_date"], ""); diaryForm.StartDate == "" {
+		diaryForm.StartDate = getDateByDays(-7)
+	}
+	if diaryForm.EndDate = strings.Join(r.Form["e_date"], ""); diaryForm.EndDate == "" {
+		diaryForm.EndDate = getDateByDays(0)
+	}
+	diaryForm.DayCount = getDaysByDate(diaryForm.StartDate, diaryForm.EndDate)
 
+	if r.Method == "POST" {
+		nd := journaldb.Diary{}
 
-    if r.Method == "POST" {
-        nd := journaldb.Diary{}
+		if oid, ok := r.Form["oid"]; ok {
+			fmt.Sscanf(strings.Join(oid, ""), "%d", &nd.Oid)
+			fmt.Printf("Update existing diary %d\n", nd.Oid)
+		} else {
+			nd.Oid = 0
+		}
+		if nd.Date = strings.Join(r.Form["date"], ""); nd.Date == "" {
+			nd.Date = getDateByDays(0)
+			fmt.Println("Using system date for diary, please make sure TZ is correct.")
+		}
+		nd.Content = strings.Join(r.Form["content"], "")
+		if strings.Join(r.Form["highlighted"], "") == "on" {
+			nd.Highlighted = true
+		} else {
+			nd.Highlighted = false
+		}
+		nd.Save(diaryForm.DBConn)
+		diaryForm.Message = "Diary of " + strings.Join(r.Form["date"], "") + " saved."
+	}
+	if oid, ok := r.Form["Edit"]; r.Method == "GET" && ok {
+		diaryForm.Mode = "Update"
+		fmt.Sscanf(strings.Join(oid, ""), "%d", &diaryForm.Diary2Update.Oid)
+		journaldb.GetDiary(diaryForm.DBConn, &diaryForm.Diary2Update)
+	}
 
-        if oid, ok := r.Form["oid"]; ok {
-          fmt.Sscanf(strings.Join(oid, ""),"%d", &nd.Oid)
-          fmt.Printf("Update existing diary %d\n", nd.Oid)
-        } else {
-          nd.Oid = 0
-        }
-        if nd.Date = strings.Join(r.Form["date"], ""); nd.Date == "" {
-            nd.Date = getDateByDays(0)
-            fmt.Println("Using system date for diary, please make sure TZ is correct.")
-        }
-        nd.Content = strings.Join(r.Form["content"], "")
-        if strings.Join(r.Form["highlighted"], "") == "on" {
-          nd.Highlighted = true
-        } else {
-          nd.Highlighted = false
-        }
-        nd.Save(diaryForm.DBConn)
-        diaryForm.Message = "Diary of " + strings.Join(r.Form["date"], "") + " saved."
-    }
-    if oid, ok := r.Form["Edit"]; r.Method == "GET" && ok {
-        diaryForm.Mode = "Update"
-        fmt.Sscanf(strings.Join(oid, ""),"%d", &diaryForm.Diary2Update.Oid)
-        journaldb.GetDiary(diaryForm.DBConn, &diaryForm.Diary2Update)
-    }
+	qStr := "SELECT oid, date, content, highlighted FROM diary WHERE date >= '" + diaryForm.StartDate + "' and date <= '" + diaryForm.EndDate + "' ORDER BY date ASC"
+	//fmt.Println(qStr)
+	rows, err := diaryForm.DBConn.Query(qStr)
+	checkErr(err)
+	diaryForm.Diary = nil
+	diaryForm.RowCount = 0
+	var diary journaldb.Diary
+	for rows.Next() {
+		var date string
+		err = rows.Scan(&diary.Oid, &date, &diary.Content, &diary.Highlighted)
+		checkErr(err)
+		t, _ := time.Parse(time.RFC3339, date)
+		diary.Date = t.Format(dateFormatShort)
+		diaryForm.Diary = append(diaryForm.Diary, diary)
+		diaryForm.RowCount++
+	}
 
-    qStr := "SELECT oid, date, content, highlighted FROM diary WHERE date >= '" + diaryForm.StartDate + "' and date <= '" + diaryForm.EndDate + "' ORDER BY date ASC"
-    //fmt.Println(qStr)
-    rows, err := diaryForm.DBConn.Query(qStr)
-    checkErr(err)
-    diaryForm.Diary = nil
-    diaryForm.RowCount = 0
-    var diary journaldb.Diary
-    for rows.Next() {
-      var date string
-      err = rows.Scan(&diary.Oid, &date, &diary.Content, &diary.Highlighted)
-      checkErr(err)
-      t, _ := time.Parse(time.RFC3339, date)
-      diary.Date = t.Format(timeFormat)
-      diaryForm.Diary = append(diaryForm.Diary, diary)
-      diaryForm.RowCount++
-    }
-
-    tmpl := template.Must(template.ParseFiles("diary.gtpl"))
-    tmpl.Execute(w, diaryForm)
-    diaryForm.Diary2Update = journaldb.Diary{}
-    diaryForm.Message = ""
+	tmpl := template.Must(template.ParseFiles("diary.gtpl"))
+	tmpl.Execute(w, diaryForm)
+	diaryForm.Diary2Update = journaldb.Diary{}
+	diaryForm.Message = ""
 }
 
-
 type TransactionForm struct {
-    DBConn *sql.DB
-    Mode string
-    Transaction []journaldb.Transaction
-    Currency []journaldb.Currency
-    Payment []journaldb.Payment
-    Bank []journaldb.Bank
-    StartDate string
-    EndDate string
-    DayCount int
-    RowCount int
+	DBConn      *sql.DB
+	Mode        string
+	Transaction []journaldb.Transaction
+	Currency    []journaldb.Currency
+	Payment     []journaldb.Payment
+	Bank        []journaldb.Bank
+	StartDate   string
+	EndDate     string
+	DayCount    int
+	RowCount    int
 }
 
 func (transactionForm *TransactionForm) Form(w http.ResponseWriter, r *http.Request) {
-    transactionForm.Mode = "Insert"
+	transactionForm.Mode = "Insert"
 
-    r.ParseForm()
+	r.ParseForm()
 
-    if transactionForm.StartDate = strings.Join(r.Form["s_date"], ""); transactionForm.StartDate == "" {
-        transactionForm.StartDate = getDateByDays(-7)
-    }
-    if transactionForm.EndDate = strings.Join(r.Form["e_date"], ""); transactionForm.EndDate == "" {
-        transactionForm.EndDate = getDateByDays(0)
-    }
-    transactionForm.DayCount = getDaysByDate(transactionForm.StartDate, transactionForm.EndDate)
+	if transactionForm.StartDate = strings.Join(r.Form["s_date"], ""); transactionForm.StartDate == "" {
+		transactionForm.StartDate = getDateByDays(-7)
+	}
+	if transactionForm.EndDate = strings.Join(r.Form["e_date"], ""); transactionForm.EndDate == "" {
+		transactionForm.EndDate = getDateByDays(0)
+	}
+	transactionForm.DayCount = getDaysByDate(transactionForm.StartDate, transactionForm.EndDate)
 
+	if r.Method == "POST" {
+		nt := journaldb.Transaction{}
 
-    if r.Method == "POST" {
-        nt := journaldb.Transaction{}
+		if oid, ok := r.Form["oid"]; ok {
+			fmt.Sscanf(strings.Join(oid, ""), "%d", &nt.Oid)
+			fmt.Println("Update existing transaction")
+		} else {
+			nt.Oid = 0
+		}
+		if nt.Date = strings.Join(r.Form["date"], ""); nt.Date == "" {
+			nt.Date = getDateByDays(0)
+			fmt.Println("Using system date for transaction, please make sure TZ is correct.")
+		}
+		nt.Item = strings.Join(r.Form["item"], "")
+		nt.Description = strings.Join(r.Form["description"], "")
+		fmt.Sscanf(strings.Join(r.Form["currency"], ""), "%d", &nt.Currency)
 
-        if oid, ok := r.Form["oid"]; ok {
-          fmt.Sscanf(strings.Join(oid, ""),"%d", &nt.Oid)
-          fmt.Println("Update existing transaction")
-        } else {
-          nt.Oid = 0
-        }
-        if nt.Date = strings.Join(r.Form["date"], ""); nt.Date == "" {
-            nt.Date = getDateByDays(0)
-            fmt.Println("Using system date for transaction, please make sure TZ is correct.")
-        }
-        nt.Item = strings.Join(r.Form["item"], "")
-        nt.Description = strings.Join(r.Form["description"], "")
-        fmt.Sscanf(strings.Join(r.Form["currency"], ""),"%d", &nt.Currency)
+		nt.Amount, _ = decimal.NewFromString(strings.Join(r.Form["amount"], ""))
 
-        nt.Amount, _ = decimal.NewFromString(strings.Join(r.Form["amount"], ""))
+		if strings.Join(r.Form["direction"], "") == "pay" {
+			nt.Pay = true
+			nt.Income = false
+		} else {
+			nt.Pay = false
+			nt.Income = true
+		}
 
-        if strings.Join(r.Form["direction"], "") == "pay" {
-          nt.Pay = true
-          nt.Income = false
-        } else {
-          nt.Pay = false
-          nt.Income = true
-        }
+		fmt.Sscanf(strings.Join(r.Form["payment"], ""), "%d", &nt.Payment)
+		fmt.Sscanf(strings.Join(r.Form["bank"], ""), "%d", &nt.Bank)
 
-        fmt.Sscanf(strings.Join(r.Form["payment"], ""),"%d", &nt.Payment)
-        fmt.Sscanf(strings.Join(r.Form["bank"], ""),"%d", &nt.Bank)
+		nt.Save(transactionForm.DBConn)
+	}
 
-        nt.Save(transactionForm.DBConn)
-    }
+	qStr := "SELECT * FROM vTransaction WHERE date >= '" + transactionForm.StartDate + "' and date <= '" + transactionForm.EndDate + "' ORDER BY date ASC"
+	rows, err := transactionForm.DBConn.Query(qStr)
+	checkErr(err)
+	transactionForm.Transaction = nil
+	transactionForm.RowCount = 0
+	var transaction journaldb.Transaction
+	for rows.Next() {
+		var date, amount string
+		err = rows.Scan(&transaction.Oid, &date, &transaction.Item, &transaction.Description, &transaction.Direction, &transaction.CurrencyPrefix, &amount, &transaction.PaymentName, &transaction.BankName)
+		checkErr(err)
+		t, _ := time.Parse(time.RFC3339, date)
+		transaction.Date = t.Format(dateFormatShort)
+		transaction.Amount, _ = decimal.NewFromString(amount)
+		transactionForm.Transaction = append(transactionForm.Transaction, transaction)
+		transactionForm.RowCount++
+	}
 
-    qStr := "SELECT * FROM vTransaction WHERE date >= '" + transactionForm.StartDate + "' and date <= '" + transactionForm.EndDate + "' ORDER BY date ASC"
-    rows, err := transactionForm.DBConn.Query(qStr)
-    checkErr(err)
-    transactionForm.Transaction = nil
-    transactionForm.RowCount = 0
-    var transaction journaldb.Transaction
-    for rows.Next() {
-      var date,amount string
-      err = rows.Scan(&transaction.Oid, &date, &transaction.Item, &transaction.Description, &transaction.Direction, &transaction.CurrencyPrefix, &amount, &transaction.PaymentName, &transaction.BankName)
-      checkErr(err)
-      t, _ := time.Parse(time.RFC3339, date)
-      transaction.Date = t.Format(timeFormat)
-      transaction.Amount, _ = decimal.NewFromString(amount)
-      transactionForm.Transaction = append(transactionForm.Transaction, transaction)
-      transactionForm.RowCount++
-    }
+	transactionForm.Currency = journaldb.GetCurrencies(transactionForm.DBConn)
+	transactionForm.Payment = journaldb.GetPayments(transactionForm.DBConn)
+	transactionForm.Bank = journaldb.GetBanks(transactionForm.DBConn)
 
-    transactionForm.Currency = journaldb.GetCurrencies(transactionForm.DBConn)
-    transactionForm.Payment = journaldb.GetPayments(transactionForm.DBConn)
-    transactionForm.Bank = journaldb.GetBanks(transactionForm.DBConn)
-
-    tmpl := template.Must(template.ParseFiles("transaction.gtpl"))
-    tmpl.Execute(w, transactionForm)
+	tmpl := template.Must(template.ParseFiles("transaction.gtpl"))
+	tmpl.Execute(w, transactionForm)
 }
 
-
-
 func checkErr(err error) {
-    if err != nil {
-        panic(err)
-    }
+	if err != nil {
+		panic(err)
+	}
 }
 
 func getDateByDays(d int) string {
-    n := time.Now().AddDate(0, 0, d)
-    return n.Format(timeFormat)
+	n := time.Now().AddDate(0, 0, d)
+	return n.Format(dateFormatShort)
 }
 
 func getDaysByDate(ts1, ts2 string) int {
-    t1, _ := time.Parse(timeFormat, ts1)
-    t2, _ := time.Parse(timeFormat, ts2)
-  return int(t2.Sub(t1) / (24 * time.Hour)) + 1
+	t1, _ := time.Parse(dateFormatShort, ts1)
+	t2, _ := time.Parse(dateFormatShort, ts2)
+	return int(t2.Sub(t1)/(24*time.Hour)) + 1
 }
 
-
 func main() {
-    dbconn := journaldb.Open("ian_journal.db")
-    diaryForm := DiaryForm {DBConn: dbconn}
-    transactionForm := TransactionForm {DBConn: dbconn}
+	flag.Parse()
+	var addrStr = *svrAddress + ":" + *svrPort
 
-    fs := http.FileServer(http.Dir("static"))
-    http.Handle("/", fs)
-    http.HandleFunc("/diary", diaryForm.Form)
-    http.HandleFunc("/transaction", transactionForm.Form)
+	dbconn := journaldb.Open(*dbFile)
+	diaryForm := DiaryForm{DBConn: dbconn}
+	transactionForm := TransactionForm{DBConn: dbconn}
 
-    caCert, err := ioutil.ReadFile("ca.crt")
-    if err != nil {
-        log.Fatal(err)
-    }
+	fs := http.FileServer(http.Dir("static"))
+	http.Handle("/", fs)
+	http.HandleFunc("/diary", diaryForm.Form)
+	http.HandleFunc("/transaction", transactionForm.Form)
 
-    caCertPool := x509.NewCertPool()
-    caCertPool.AppendCertsFromPEM(caCert)
+	caCert, err := ioutil.ReadFile(*clientCaCertFile)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    // Setup HTTPS client
-    tlsConfig := &tls.Config {
-        ClientCAs: caCertPool,
-        // NoClientCert
-    		// RequestClientCert
-    		// RequireAnyClientCert
-    		// VerifyClientCertIfGiven
-    		// RequireAndVerifyClientCert
-    		ClientAuth: tls.RequireAndVerifyClientCert,
-    }
-    tlsConfig.BuildNameToCertificate()
+	caCertPool := x509.NewCertPool()
+	caCertPool.AppendCertsFromPEM(caCert)
 
-    server := &http.Server {
-		Addr:      ":8086",
+	// Setup HTTPS client
+	tlsConfig := &tls.Config{
+		ClientCAs: caCertPool,
+		// NoClientCert
+		// RequestClientCert
+		// RequireAnyClientCert
+		// VerifyClientCertIfGiven
+		// RequireAndVerifyClientCert
+		ClientAuth: tls.RequireAndVerifyClientCert,
+	}
+	tlsConfig.BuildNameToCertificate()
+
+	server := &http.Server{
+		Addr:  addrStr,
 		TLSConfig: tlsConfig,
-    }
+	}
 
-    server.ListenAndServeTLS("server.crt", "server.key")
+journaldb.SaveMessage(dbconn, fmt.Sprintf("Serving requests on https://%s/.", addrStr))
+server.ListenAndServeTLS(*serverCertFile, *serverKeyFile)
 
 }
